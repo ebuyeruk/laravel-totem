de91562e3d1980dd463873733953961819ce34aa
Chris Cutts <chris.cutts@ebuyer.com>
2025-06-12 11:59:05 +0100

    Update footer copyright

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
     <div class="uk-container">
-        <p class="uk-text-center">  © <a target="_blank" href="https://twitter.com/@roshangautam">Roshan Gautam</a>. All rights reserved.</p>
+        <p class="uk-text-center">© 2025 Ebuyer UK Ltd. All rights reserved. <a href="https://www.ebuyer.com" target="_blank" rel="noopener noreferrer">Visit Ebuyer.com</a></p>
     </div>
 </footer>
